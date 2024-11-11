0ebe87be7a7517f14d3ad36d83578039d5f24072
Yasin Yaşar <156111299+sahici@users.noreply.github.com>
2024-11-11 18:25:53 +0300

    Add files via upload

---
+++ lib/color.py
@@ -0,0 +1,10 @@
+# Inquiry v1.0 color
+# Signature: Yasin Yaşar
+
+# Renk kodları
+not_found = "\033[38;2;255;49;49m"  # #FF5733
+found = "\033[38;2;255;229;180m"  # #FFE5B4
+yellow_wpcrawl = "\033[38;2;255;193;110m"  #E1C16E
+ascii_art = "\033[38;2;255;87;51m" #FF5733
+cobalt_blue = "\033[38;2;0;71;171m"  #0047AB
+reset_color = "\033[0m"  # Renk sıfırlama
