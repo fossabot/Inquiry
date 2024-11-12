0e219aaf511b63fb93400e0255e411b090bc2911
Yasin Yaşar <156111299+sahici@users.noreply.github.com>
2024-11-13 00:18:46 +0300

    Update MoodleCrawl.py

--- modules/MoodleCrawl.py
+++ modules/MoodleCrawl.py
@@ -6,14 +6,11 @@ from lib.color import yellow_wpcrawl, not_found, reset_color
 
 def crawl(url):
     try:
-        # Siteye istek gönderiyoruz
         istek = requests.get(f"https://{url}/lib/upgrade.txt")
         
-        # Eğer başarılı bir cevap alırsak
         if istek.status_code == 200:
             response_text = istek.text
-            
-            # "===" arasında bir version kodu arıyoruz
+           
             match = re.search(r"===\s*(.*?)\s*===", response_text)
             
             if match:
@@ -21,13 +18,10 @@ def crawl(url):
                 print(f"{yellow_wpcrawl}[+] Version Kodu: {version_code}{reset_color}")
             
             else:
-                # Version kodu bulunamazsa
                 print(f"{not_found}[!] Version Kodu Tespit Edilemedi{reset_color}")
         
         else:
-            # HTTP isteği başarısız olduysa
             print(f"{not_found}[!] İstek başarısız oldu, HTTP Durum Kodu: {istek.status_code}{reset_color}")
     
     except requests.exceptions.RequestException as e:
-        # Hata oluşursa
         print(f"{not_found}[!] Bir Hata oluştu: {e}{reset_color}")
